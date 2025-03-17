a055dd68815c3de87cb1d8df48dc11055a3589a2
saichandu208 <thatipallysaichandu123@gamil.com>
2025-03-17 22:31:26 +0530

    Third commit

--- src/test/java/com/SaiChan/TestCases/Sample.java
+++ src/test/java/com/SaiChan/TestCases/Sample.java
@@ -1,5 +1,18 @@
 package com.SaiChan.TestCases;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+public class Sample {
 
-public class Sample
-{
+    @Test
+    public void test_get_Request() {
+    RestAssured
+            .given()
+            .baseUri(  "https://restful-booker.herokuapp.com")
+            .basePath( "/booking/1")
+            .when()
+            .get()
+            .then().log().all()
+            .statusCode(200);
+
+}
 }
